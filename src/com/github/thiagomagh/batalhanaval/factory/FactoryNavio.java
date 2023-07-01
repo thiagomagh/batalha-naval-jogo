89034274b5138cd16f281b2a61d9992863439306
Thiago Magalhães <128939957+thiagomagh@users.noreply.github.com>
2023-06-30 21:46:19 -0300

    Update license notice for each source file

--- src/com/github/thiagomagh/batalhanaval/factory/FactoryNavio.java
+++ src/com/github/thiagomagh/batalhanaval/factory/FactoryNavio.java
@@ -1,21 +1,10 @@
 /*
  * Copyright (C) 2023 by Thiago Magalhães <https://github.com/thiagomagh>
+ *  
+ * This program is licensed under the GNU GPL v2.0 or any later version.
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Original source code: https://github.com/thiagomagh/batalha-naval-jogo
+ * For more details, see COPYING file or 
+ * <https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt>
  */
 
 package com.github.thiagomagh.batalhanaval.factory;
